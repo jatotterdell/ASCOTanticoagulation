d9cdafa2a573df306e9ed18ad3c30c1872f00ae2
James Totterdell <jatotterdell@gmail.com>
2022-06-20 12:12:14 +1000

    Add function to read the site activation data provided by management group

--- R/read_raw_data.r
+++ R/read_raw_data.r
@@ -256,6 +256,35 @@ read_deviation_file <- function(fn) {
 }
 
 
+read_site_activation_file <- function() {
+  active <- read_csv(file.path(
+    ASCOT_DATA_RAW, "ASCOTADAPT_SiteActivationDates_20220620.csv"
+  ),
+  col_types = cols(
+    `Site activated on protocol version 3.0` = col_date(format = "%d-%b-%y"),
+    `Site activated on protocol version 5.0 (Therapeutic AC)` = col_date(format = "%d-%b-%y")
+  )) %>%
+    rename(
+      state = STATE,
+      site = `Site Code`,
+      health_service = `Health service`,
+      hospname_hospaddress = `Hospital name/address`,
+      active_domains = `Domains Active`,
+      active_v3 = `Site activated on protocol version 3.0`,
+      active_v5 = `Site activated on protocol version 5.0 (Therapeutic AC)`
+    ) %>%
+    mutate(
+      country = case_when(
+        state == "India" ~ "IN",
+        state == "Nepal" ~ "NP",
+        state == "NZ" ~ "NZ",
+        TRUE ~ "AU"
+      )
+    )
+  return(active)
+}
+
+
 ## Manually created files from trial management group ----
 
 
